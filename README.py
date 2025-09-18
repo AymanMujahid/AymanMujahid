c841556f872d667342e3292220a0caf8bf7ef0b8
Ayman Mujahid <68431884+AymanMujahid@users.noreply.github.com>
2025-09-18 16:56:30 +0300

    Update README.py

--- README.py
+++ README.py
@@ -1,30 +1,26 @@
-
-<p align="center">
-  <img src="https://raw.githubusercontent.com/aymanmujahid/aymanmujahid/main/banner.gif" alt="Generative AI Banner" width="100%" />
-</p>
-
-# 👋 Hey, I'm Ayman Mujahid  
-
-💻 **Software Engineer – Back-End & Generative AI Focused**  
-🚀 *Passionate about Building with Generative AI & Exploring Intelligent Solutions*  
-🏢 Currently at **SBS**
-
----
-
-### 🧠 
-```python
 from earth import Human
 
 class ItMe(Human):
-    def __init__(self):
-        self.first_name = "Ayman"
-        self.last_name = "Mujahid"
-        self.work_where = "SBS"
-        self.work_what = "Software Engineer – Back-End & Generative AI Focused"
+    def __init__(self,
+                 first_name: str = "Ayman",
+                 last_name: str = "Mujahid",
+                 work_where: str = "SBS",
+                 work_what: str = (
+                     "Software Engineer – Back-End & Generative AI Focused | "
+                     "Passionate About Building with Generative AI | "
+                     "Exploring Intelligent Solutions")):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.work_where = work_where
+        self.work_what = work_what
 
     def hi(self):
-        print(f"hey 👋, i'm {self.first_name} {self.last_name}")
+        print(f"hey 👋, i'm {self.first_name.capitalize()} {self.last_name.capitalize()}")
         print(f"currently i work in {self.work_what} @ {self.work_where}")
 
 me = ItMe()
 me.hi()
+
+
+hey 👋, i'm Ayman Mujahid
+currently i work in Software Engineer – Back-End & Generative AI Focused | Passionate About Building with Generative AI | Exploring Intelligent Solutions @ SBS
